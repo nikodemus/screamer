3b7d7d2974197f66545788986023463c18ff003c
Nikodemus Siivola <nikodemus@sb-studio.net>
2011-03-21 19:25:31 +0200

    ATTACH-NOTICER! can now attach the same noticer to multiple things
      Micro-optimizes some things a bit, as we don't now call
      the same noticer twice for eg. (ASSERT! (=V X Y)).

--- screamer.lisp
+++ screamer.lisp
@@ -3377,8 +3377,10 @@ nil."
                                  (cons noticer (variable-noticers x)))))
                   (attach-noticer!-internal noticer (variable-value x))))))
 
-(defun attach-noticer! (noticer x)
-  (attach-noticer!-internal noticer x)
+(defun attach-noticer! (noticer &rest things)
+  (declare (dynamic-extent things))
+  (dolist (x things)
+    (attach-noticer!-internal noticer x))
   (funcall noticer))
 
 (defun run-noticers (x)
@@ -4739,24 +4741,21 @@ nil."
   (assert!-numberpv y)
   (let ((x (variablize x))
         (y (variablize y)))
-    (attach-noticer! #'(lambda () (=-rule x y)) x)
-    (attach-noticer! #'(lambda () (=-rule x y)) y)))
+    (attach-noticer! #'(lambda () (=-rule x y)) x y)))
 
 (defun assert!-<=v2 (x y)
   (assert!-realpv x)
   (assert!-realpv y)
   (let ((x (variablize x))
         (y (variablize y)))
-    (attach-noticer! #'(lambda () (<=-rule x y)) x)
-    (attach-noticer! #'(lambda () (<=-rule x y)) y)))
+    (attach-noticer! #'(lambda () (<=-rule x y)) x y)))
 
 (defun assert!-<v2 (x y)
   (assert!-realpv x)
   (assert!-realpv y)
   (let ((x (variablize x))
         (y (variablize y)))
-    (attach-noticer! #'(lambda () (<-rule x y)) x)
-    (attach-noticer! #'(lambda () (<-rule x y)) y)))
+    (attach-noticer! #'(lambda () (<-rule x y)) x y)))
 
 (defun assert!-/=v2 (x y)
   (assert!-numberpv x)
@@ -4765,8 +4764,7 @@ nil."
         (y (variablize y)))
     ;; note: Got rid of the nondeterministic version that called the
     ;;       nondeterministic version of /=-RULE.
-    (attach-noticer! #'(lambda () (/=-rule x y)) x)
-    (attach-noticer! #'(lambda () (/=-rule x y)) y)))
+    (attach-noticer! #'(lambda () (/=-rule x y)) x y)))
 
 ;;; Lifted Type Functions
 
